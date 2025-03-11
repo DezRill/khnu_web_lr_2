0972d64fd240d7a5185cf7080e445975849c8ee2
a106-2 <khnu_a106_2@gmail.com>
2025-03-11 12:21:05 +0200

    edit styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burger = document.querySelector('.header-burger');
     const menu = document.querySelector('.mobile-menu');
+    const header = document.querySelector('header');
 
     burger.addEventListener('click', function () {
         this.classList.toggle('active');
         menu.classList.toggle('active');
+    });
+
+    document.addEventListener('scroll', function(e) {
+        if (window.scrollY === 0) {
+            header.style.padding = '20px 0';
+        } else {
+            header.style.padding = '0';
+        }
     });
 });
